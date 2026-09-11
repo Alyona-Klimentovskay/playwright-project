bd5c4da143aaa4277441fb0562f5b6efe1f4f808
eklimentovska <elena.klimentovska@gmail.com>
2026-09-11 15:23:37 +0200

    Changed customer.setup.js

--- tests/customer.setup.js
+++ tests/customer.setup.js
@@ -1,27 +1,22 @@
 import { test as setup, expect } from '@playwright/test';
 import { LoginPage } from '../lib/pages/LoginPage.js';
-import { PersonalAccountPage } from '../lib/pages/PersonalAccountPage.js';
+
 
 const customerAuthFile = 'playwright/.auth/existing-customer.json';
 
 setup('Login as existing customer and save storageState', async ({ page }) => {
-      
-    const loginPage = new LoginPage(page);
-    const myAccountPage = new PersonalAccountPage(page);
-    await loginPage.navigate('/auth/login'); 
-
-    const loginResponsePromise = page.waitForResponse(
-        response => response.url().includes('/users/login') && response.status() === 200,
-        { timeout: 15000 }
-    );
     
+    
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate('/auth/login');   
     await loginPage.customerLogin(
         process.env.EXISTING_CUSTOMER_EMAIL,
         process.env.EXISTING_CUSTOMER_PASSWORD
     );
-    await loginResponsePromise;
-    await expect(page).toHaveURL('/account', { timeout: 10000 });
-    await myAccountPage.checkMyAccountPageIsLoaded();
+    await expect(loginPage.page).toHaveURL('/account');
     await page.context().storageState({ path: customerAuthFile });
 });
+
+
 
