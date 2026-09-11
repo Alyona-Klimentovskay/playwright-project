6d7f6780b159b6708fc91960f4ba762e57a4e7f6
eklimentovska <elena.klimentovska@gmail.com>
2026-09-11 14:23:47 +0200

    New changes

--- tests/customer.setup.js
+++ tests/customer.setup.js
@@ -1,27 +1,27 @@
 import { test as setup, expect } from '@playwright/test';
 import { LoginPage } from '../lib/pages/LoginPage.js';
-
+import { PersonalAccountPage } from '../lib/pages/PersonalAccountPage.js';
 
 const customerAuthFile = 'playwright/.auth/existing-customer.json';
 
 setup('Login as existing customer and save storageState', async ({ page }) => {
-    
-    if (!process.env.EXISTING_CUSTOMER_EMAIL || !process.env.EXISTING_CUSTOMER_PASSWORD) {
-        throw new Error(
-            `КРИТИЧЕСКАЯ ОШИБКА: Переменные окружения для Customer пусты на CI! ` +
-            `Проверьте GitHub Secrets. EMAIL существует: ${!!process.env.EXISTING_CUSTOMER_EMAIL}, ` +
-            `PASSWORD существует: ${!!process.env.EXISTING_CUSTOMER_PASSWORD}`
-        );
-    }
-    
+      
     const loginPage = new LoginPage(page);
+    const myAccountPage = new PersonalAccountPage(page);
+    await loginPage.navigate('/auth/login'); 
 
-    await loginPage.navigate('/auth/login');   
+    const loginResponsePromise = page.waitForResponse(
+        response => response.url().includes('/users/login') && response.status() === 200,
+        { timeout: 15000 }
+    );
+    
     await loginPage.customerLogin(
         process.env.EXISTING_CUSTOMER_EMAIL,
         process.env.EXISTING_CUSTOMER_PASSWORD
     );
-    await expect(loginPage.page).toHaveURL('/account');
+    await loginResponsePromise;
+    await expect(page).toHaveURL('/account', { timeout: 10000 });
+    await myAccountPage.checkMyAccountPageIsLoaded();
     await page.context().storageState({ path: customerAuthFile });
 });
 
